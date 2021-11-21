5c8432a7fb7b9f98e4860eaf44df6a9be77820ce
sagedemage <salsaeed999@gmail.com>
2021-11-21 16:46:29 -0500

    Added testing by csv file implementing pandas

--- test/addition_test.py
+++ test/addition_test.py
@@ -1,5 +1,6 @@
 """Testing Addition"""
 
+import pandas as pd
 from calculation.addition import Addition
 
 
@@ -11,3 +12,21 @@ def test_addition():
     result = addition.get_result()
     # Assert
     assert result == 3
+
+
+def test_addition_pandas():
+    """Testing subtraction method for two values"""
+    path = "test/test_data/addition.csv"
+    # Read in the cvs file
+    dataframe = pd.read_csv(path)
+
+    # Read each column of the table
+    value1 = dataframe["value_1"]
+    value2 = dataframe["value_2"]
+    result = dataframe["result"]
+
+    for i in range(result.size):
+        # Arrange
+        addition = Addition(value1[i], (value2[i],))
+        # Act and Assert
+        assert addition.get_result() == result[i]
